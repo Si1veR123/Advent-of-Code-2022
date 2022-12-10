8403d06c3ea2d4889bda10b232b586695e66e0d0
Connor George <connorgeorgeem@aol.com>
2022-12-10 16:09:41 +0000

    Day 3

---
+++ Rust/src/bin/3.rs
@@ -0,0 +1,70 @@
+use std::collections::HashSet;
+use std::collections::hash_map::RandomState;
+use std::fs::File;
+use std::io::Read;
+
+fn get_priority(c: char) -> u8 {
+    if c.is_uppercase() {
+        return (c as u8) - 38
+    } else {
+        return (c as u8) - 96
+    }
+}
+
+fn main() {
+    let mut file = File::open("../data/3.txt").expect("Data file not found");
+    let mut string_buf = String::new();
+    file.read_to_string(&mut string_buf).expect("Reading file failed");
+    let lines: Vec<&str> = string_buf.split("\r\n").collect();
+
+    println!("Part 1");
+    let sum_priorities: u32 = lines.iter().map(
+        |&x| {
+            let chars: Vec<char> = x.chars().collect();
+            let half_point = (chars.len()/2) as usize;
+            let first_chars_slice = &chars[..half_point];
+            let second_chars_slice = &chars[half_point..];
+            let first_compartment: HashSet<&char, RandomState> = HashSet::from_iter(first_chars_slice.iter());
+            let second_compartment: HashSet<&char, RandomState> = HashSet::from_iter(second_chars_slice.iter());
+
+            let &&item = first_compartment.intersection(&second_compartment).next().unwrap();
+            get_priority(item) as u32
+        }
+    ).sum();
+    println!("{}", sum_priorities);
+
+    println!("Part 2");
+    let mut accumulated_groups = vec![];
+
+    // not very efficient as new vector is heap allocated for each element
+    lines.iter()
+        .fold(vec![], |acc, &x| {
+            let mut next_vec = acc.clone();
+            next_vec.push(x);
+
+            if next_vec.len() == 3 {
+                accumulated_groups.push(next_vec);
+                return vec![]
+            }
+            next_vec
+        });
+    
+    let badges_sum: u32 = accumulated_groups.iter().map(
+        |x| {
+            // assumed length of x is always 3, or will panic
+            let sets: [HashSet<char, RandomState>; 3] = [
+                HashSet::from_iter(x.get(0).unwrap().chars()),
+                HashSet::from_iter(x.get(1).unwrap().chars()),
+                HashSet::from_iter(x.get(2).unwrap().chars()),
+            ];
+
+            let first_intersect = HashSet::from_iter(sets[1].intersection(&sets[2]).cloned());
+            let all_intersect = sets[0].intersection(&first_intersect);
+
+            let badge = all_intersect.last().unwrap().clone();
+            get_priority(badge) as u32
+        }
+    ).sum();
+
+    println!("{}", badges_sum);
+}
