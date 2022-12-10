97f7a41938f74999dfcf67425cd009b6be1da39c
Connor George <connorgeorgeem@aol.com>
2022-12-10 16:21:19 +0000

    shortened day 3

--- Python/3.py
+++ Python/3.py
@@ -3,5 +3,4 @@
 with open("../data/3.txt") as file:
     lines = file.read().splitlines()
 get_priority = lambda x: ord(x)-38 if x.isupper() else ord(x)-96
-print("Part 1\n", sum([get_priority(list(set(line[:int(len(line)/2)]).intersection(set(line[int(len(line)/2):])))[0]) for line in lines]))
-print("Part 2\n", sum([get_priority(list(set.intersection(*[set(x) for x in lines[n*3:n*3+3]]))[0]) for n in range(int(len(lines)/3))]))
+print("Part 1\n", sum([get_priority(list(set(line[:int(len(line)/2)]).intersection(set(line[int(len(line)/2):])))[0]) for line in lines]), "\n", "Part 2\n", sum([get_priority(list(set.intersection(*[set(x) for x in lines[n*3:n*3+3]]))[0]) for n in range(int(len(lines)/3))]))
